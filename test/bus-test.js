cdc28bc1f3c5989b3168ec8a7779b017b31bbd87
Adam Rice <adamcoltonrice@gmail.com>
2016-11-03 14:43:12 -0600

    bus tests passing for location and functions

---
+++ test/bus-test.js
@@ -0,0 +1,68 @@
+var assert = require('chai').assert;
+var Bus = require('../lib/bus');
+
+describe('Bus', function() {
+  it('should be an object', function() {
+    var bus = new Bus();
+    assert.isObject(bus);
+  });
+
+  it('should have a x coordinate of 540 by default', function() {
+    var terminator = new Bus();
+    assert.equal(terminator.x, 540);
+  });
+
+  it('should have a y coordinate of 280 by default', function() {
+    var jon = new Bus();
+    assert.equal(jon.y, 280);
+  });
+
+  it('should have a height of 120 by default', function() {
+    var terminator = new Bus();
+    assert.equal(terminator.height, 120);
+  });
+
+  it('should have a width of 30 by default', function() {
+    var jon = new Bus();
+    assert.equal(jon.width, 30);
+  });
+
+  it('should have a draw function', function(){
+    var jon = new Bus();
+    assert.isFunction(jon.draw);
+  });
+
+  it('should have a moveLeft function', function(){
+    var jon = new Bus();
+    assert.isFunction(jon.moveLeft);
+  });
+
+  it('should decrement the x value by 2 if moveLeft is called', function() {
+    var bus = new Bus();
+    bus.moveLeft();
+    assert.equal(bus.x, 538);
+  });
+
+  it('should place the bus at an x value of 620 if moveLeft is called while bus.x is less than or equal to -120', function() {
+    var bus = new Bus(-120);
+    bus.moveLeft();
+    assert.equal(bus.x, 620);
+  });
+
+  it('should have a moveRight function', function(){
+    var jon = new Bus();
+    assert.isFunction(jon.moveRight);
+  });
+
+  it('should increment the x value by 1.3 if moveRight is called', function() {
+    var bus = new Bus();
+    bus.moveRight();
+    assert.equal(bus.x, 541.3);
+  });
+
+  it('should place the bus at an x value of -120 if moveRight is called while bus.x is less than or equal to 700', function() {
+    var bus = new Bus(700);
+    bus.moveRight();
+    assert.equal(bus.x, -120);
+  });
+});
--- test/index.js
+++ test/index.js
@@ -2,3 +2,5 @@ require('./frog-test.js');
 require('./pad-test.js');
 require('./zone-test.js');
 require('./car-test.js');
+require('./bus-test.js');
+require('./bike-test.js');
